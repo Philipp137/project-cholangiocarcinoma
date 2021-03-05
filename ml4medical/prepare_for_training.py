0014d8d22c01e4c89e432160a477eddbab53746d
Philipp Krah <philipp.l.krah@campus.tu-berlin.de>
2021-03-05 09:58:14 +0100

    added prepare script vor exporting tiles with qupath annotations

--- ml4medical/prepare_for_training.py
+++ ml4medical/prepare_for_training.py
@@ -13,12 +13,14 @@ import glob
 import os
 import pandas as pd
 import numpy as np
+import pathlib
+from qupath_utils import create_qupath_project_file, create_qupath_classes_file
 
 ## Define the folders and files
 IO_paths = {"source": '/run/media/phil/Elements/iCCA/',          # raw images, i.e. *tiff files
            "annotations": '/run/media/phil/Elements/qupath/',   # folder with annotations of pathologist
            "labels": '/run/media/phil/Elements/CCC-labels.csv', # csv data with id of the tiff image and label (pos, neg)
-           "work" : '/run/media/phil/Elements/work/',           # work folder for intermediate results and files
+           "work" : '/run/media/phil/Elements/work_qupath/',           # work folder for intermediate results and files
            "ML-data":   '/run/media/phil/Elements/data/',}       # data for neuronal network to train and test
 
 ##
@@ -27,7 +29,7 @@ fraction_train = 0.8
 ## choose a method to export tiles
     # method = 'pyhist' uses pyhist for image segementation and exporting tiles without human help (pathologist)
     # method = 'qupath' exports tiles from pathologist defined areas
-method = ""
+method = "qupath"
 
 if method == "pyhist":
     ## loop over all scans in the source folder
@@ -42,16 +44,39 @@ if method == "pyhist":
         if success !=0: print("Segmentation of %s did not work! Stopping script" % filename); break
 
 elif method == "qupath":
-    pass
-    # 1. create folder for each scan (*.tif)
-
-    # 2. put in the basic blueprint of a qupath project
-    # (with specific folder structure project.qpproj file etc.)
-
-    # 3. copy annotations for each image in the folder
+    i = 0
+    for filename in sorted(glob.glob(IO_paths["source"]+'**/*.tif', recursive=True)):
+        i = i + 1
+        # %-----------------------------------------------------------
+        # 1. create folder for each scan (*.tif) with pathology number
+        file = os.path.basename(filename)                       # filename of tiff without path
+        ID, extension = os.path.splitext(file)                  # filename split into name and extension .tiff
+        project_directory = IO_paths["work"] + ID
+        if not os.path.exists(project_directory):
+            os.makedirs(project_directory)
+            print(f"Creating project dir in: {project_directory:s}")
+        # %-----------------------------------------------------------
+        # 2. put in the basic blueprint of a qupath project
+        # (with specific folder structure project.qpproj file etc.)
+        # 2a folder structure:
+        pathlib.Path(project_directory+'/data').mkdir(parents=True, exist_ok=True)
+        pathlib.Path(project_directory + '/data/2/').mkdir(parents=True, exist_ok=True)
+        pathlib.Path(project_directory + '/data/1/').mkdir(parents=True, exist_ok=True)
+        pathlib.Path(project_directory + '/classifiers/').mkdir(parents=True, exist_ok=True)
+        # 2b create the project file "project.qproj" in project_directory and include the path to the tiff file
+        txt, success = create_qupath_project_file(project_directory, filename, file)
+        # 2c create json file of classes (stroma , tumor, etc.)
+        create_qupath_classes_file(project_directory)
+        assert (success > 0) , " no project file written"
+        # %-----------------------------------------------------------
+        # 3. copy annotations for each image in the folder
+        annotations= IO_paths["annotations"] + "/" + str(i)+"/*"
+        command = "cp %s %s" %(annotations, project_directory+'/data/1/')
+        success = os.system(command)
 
     # 4. run groovy script to export tiles from the annotations
 
+assert ( False)
 
 df = pd.read_csv(IO_paths["labels"])
 test = {"pos": IO_paths["ML-data"] + "/test/pos/", "neg": IO_paths["ML-data"] + "/test/neg/"}
