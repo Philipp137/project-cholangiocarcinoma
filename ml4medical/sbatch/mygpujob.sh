1eb4df6b118cff9936aeed4c6104769d1900a487
nb671233 <nb671233@login-g.hpc.itc.rwth-aachen.de>
2021-03-25 17:25:34 +0100

    some minor changes

--- ml4medical/sbatch/mygpujob.sh
+++ ml4medical/sbatch/mygpujob.sh
@@ -5,15 +5,17 @@
 #SBATCH --error=CCC-GPU_id-%J.out
 #SBATCH --mail-type=END
 #SBATCH --mail-user=steffen.nitsch@live.de
-#SBATCH --nodes=2
-#SBATCH --ntasks=2
-#SBATCH --cpus-per-task=1
-#SBATCH --ntasks-per-node=1
-###SBATCH --distribution=cyclic:cyclic
+#SBATCH --nodes=1
+##SBATCH --ntasks=1
+##SBATCH --cpus-per-task=4		        ## cpus 24 for pascal 48 for volta			
+#SBATCH --ntasks-per-node=1   			## ntasks should be same as number of gpus	
+#SBATCH --mem=0 				## 32 GB for gpus
 ###SBATCH --mem-per-cpu=100mb
-#SBATCH --gres=gpu:pascal:2
-#SBATCH --time=00:10:00
+#SBATCH --gres=gpu:volta:1
+#SBATCH --time=02:00:00
 
+
+echo "cpus per task:" $SBATCH_CPUS_PER_TASK
 source ~/.zshrc
 conda activate ml4medical
 srun python ../../main.py
--- start_tensorbaord.py
+++ start_tensorbaord.py
@@ -1,6 +1,6 @@
 import os
 import webbrowser
 
-log_file_dir = './lightning_logs/'
-webbrowser.open('http://localhost:6006')
-os.system('tensorboard --logdir=' + log_file_dir)
+log_file_dir = '/home/nb671233/project-cholangiocarcinoma/ml4medical/sbatch/lightning_logs/'
+webbrowser.open('http://localhost:8008')
+os.system('tensorboard --logdir=' + log_file_dir + ' --port=8008')
